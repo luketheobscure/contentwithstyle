9523318b0a351c07dd6801fd3c29b220eaa82768
pascalopitz <contact@pascalopitz.com>
2011-03-18 08:56:37 +0000

    included comments ... shame they had to be markup

--- exporter/exporter.php
+++ exporter/exporter.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once 'markdownify/markdownify.php';
-
 class Exporter {
     
     const DBHOST = '127.0.0.1'; 
@@ -42,24 +40,16 @@ class Exporter {
         echo 'Exporting to ' . $this->path;
         echo "\n";
 
-        $query = $this->getQuery();
-        $result = mysql_query($query);
-
-        if (!$result) {
-            die('Invalid query: ' . mysql_error());
-        }
+        $posts = $this->getPosts();
         
-        while ($row = mysql_fetch_assoc($result)) {
+        foreach ($posts as $row) {
             $this->createFile($row);
         }
 
-        mysql_free_result($result);
         echo "\n";
     }
 
     private function  createFile($row) {
-        $this->md = new Markdownify();
-        
         foreach($row as $k=>$v) {
             $row[$k] = stripslashes($v);
         };
@@ -79,13 +69,117 @@ class Exporter {
         $str .= "categories : \n";
         $str .= "   - " . date('F Y', strtotime($row['published'])) . "\n";
         $str .= "   - \"" . $row['first_name'] . " " . $row['last_name'] . "\"\n";
+        $str .= "comments : |\n";
+        $str .=  '    ' . str_replace("\n", "\n    ", $this->getCommentsHtml($row['id']));
+        $str .= "\n";
         $str .= "---\n\n";
 
         $str .= $row['text'];
         
+        // $str .= '<div class="comments" id="comments">';
+        // $str .= "<h2>Comments</h2>";
+        // 
+        // if(empty($comments)) {
+        //     $str .= '<p class="odd">No comments have been left.</p>';
+        // } else {
+        //     $str .= '<ul class="comments-list">';
+        // 
+        // }    
+        // $str .= '</div>';
+        
         $dir = $this->path . '/_posts';
         mkdir($dir);
         file_put_contents($dir . '/'. date('Y-m-d', strtotime($row['published'])) . '-' . $row['url_token'] . '.html', $str);
+    }
+    
+    private function getPosts() {
+        echo 'fetching posts';
+        echo "\n";
+        
+        $query = $this->getQuery();
+        $result = mysql_query($query);
+
+        if (!$result) {
+            die('Invalid query: ' . mysql_error());
+        }
+        
+        $posts = array();
+
+        while ($row = mysql_fetch_assoc($result)) {
+            $posts[] = $row;
+        }
+
+        mysql_free_result($result);
+        echo 'fetching posts done';
+        echo "\n";
+
+        return $posts;
+    }
+    
+    private function getCommentsHtml($id) {
+        $str = '';
+
+        $comments = $this->getComments($id);
+        $cnt = 0;
+        foreach($comments as $comment) {
+            $cnt++;
+            $class = (($cnt % 2) == 0) ? 'even' : 'odd';
+
+    		if(!empty($comment['twitter_screen_name'])) {
+    			$class .= ' twitter';
+    		}
+
+            $str .= '<li class="' . $class . '" id="comment-' . $comment['id'] . '">';
+            $str .= "\n";
+            $str .= '<div class="comment-text">' . $comment['text'] . '</div>';
+            $str .= "\n";
+            $str .= '<p class="comment-info">by ';
+            $str .= "\n";
+                     
+            if(!empty($comment['twitter_screen_name'])) {
+                $str .= '<a href="http://www.twitter.com/' . $comment['twitter_screen_name'] . '" rel="nofollow">' . $comment['twitter_screen_name'] . '</a>';
+            } elseif(!empty($comment['website']) && $comment['website'] != 'http://') {    
+                $str .= '<a href="';
+                $str .=  (strpos ($comment['website'], 'http://') !==0 && strpos ($comment['website'], 'https://')!==0) ?
+                    'http://'.$comment['website'] : $comment['website'];
+                $str .= '" rel="nofollow">' .  $comment['username'] . '</a>'; 
+            } else {    
+                $str .= $comment['username'];
+            }
+                    
+            $str .= 'on ' . date('F j Y, H:i', strtotime($comment['created']));
+            $str .= '<a href="#comment-' . $comment['id'] .'">#</a>';
+            $str .= "\n";
+            $str .= '</p>';
+            $str .= "\n";
+            $str .= '</li>';
+            $str .= "\n";
+        }
+        
+        return $str;
+    }
+    
+    private function getComments($id) {
+        echo 'fetching comments for '.$id;
+        echo "\n";
+        $query = $this->getCommentsQuery($id);
+        $result = mysql_query($query);
+
+        if (!$result) {
+            die('Invalid query: ' . mysql_error());
+        }
+        
+        $comments = array();
+        
+        while ($row = mysql_fetch_assoc($result)) {
+            $comments[] = $row;
+        }
+
+        mysql_free_result($result);
+        mysql_free_result($result);
+        echo 'fetching comments for '.$id.' done';
+        echo "\n";
+        return $comments;
     }
     
     private function getQuery() {
@@ -97,6 +191,22 @@ class Exporter {
             INNER JOIN authors a ON c.author_id = a.id
             WHERE c.status_id = 4
             AND listed = 1
+        ";
+        
+        return $query;
+    }
+
+
+    private function getCommentsQuery($id) {
+        $query = "SELECT 
+                c.*, 
+                u.name, 
+                u.email,
+                u.website
+            FROM comments c
+            INNER JOIN users u ON c.user_id = u.id
+            WHERE c.status_id = 4
+            AND c.content_id = $id
         ";
         
         return $query;
